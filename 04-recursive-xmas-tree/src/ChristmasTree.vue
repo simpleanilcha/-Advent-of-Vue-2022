e7d47dee5eecaa88a0f5f9200a9636703afc95fb
Anil Maharjan <simpleanilcha@gmail.com>
2023-01-03 12:04:07 +0545

    solution: 04 day - recursion xmas tree with v-for

--- 04-recursive-xmas-tree/src/ChristmasTree.vue
+++ 04-recursive-xmas-tree/src/ChristmasTree.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
-    <div class="flex flex-row justify-center">
-      <!-- This div is styled for the tree sections, but it's not complete -->
-      <div class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+    <div v-for="i in size" class="flex justify-center">
+      <div v-for="j in i" class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
     </div>
   </div>
 </template>
